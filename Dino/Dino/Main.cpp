b69cb8e5b2f02eff9983ce277fa0b3298a9d432d
QazmoQwerty <sdonmeiri@gmail.com>
2019-07-29 02:28:27 +0300

    added basic main for testing (input from file)

--- Dino/Dino/Main.cpp
+++ Dino/Dino/Main.cpp
@@ -1,5 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <sstream> 
 
-void main()
+#include "Lexer.h"
+
+int main()
 {
+	std::ifstream t("Text.txt");
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+	std::string str = buffer.str();
+	std::cout << str << std::endl;
 
+	Lexer::setup();
+	for (Token * t : Lexer::lex(str))
+	{
+		printToken(t);
+	}
+
+	system("pause");
 }
