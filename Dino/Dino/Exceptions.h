0fbd5fdea3a1ba47e6ad621532086edbcb98187c
QazmoQwerty <sdonmeiri@gmail.com>
2019-07-31 01:36:35 +0300

    added ExceptioType variable to custom exception type

--- Dino/Dino/Exceptions.h
+++ Dino/Dino/Exceptions.h
@@ -10,13 +10,15 @@ using std::string;
 using std::exception;
 
 class DinoException : public exception {
+	ExceptionType _type;
 	int _line;
 	const string _info;
 
 public:
-	DinoException(const char* msg, int line, const string info = "") : 
-		std::exception(msg), _line(line), _info(info) {}
+	DinoException(const char* msg, ExceptionType type, int line, const string info = "") :
+		std::exception(msg), _type(type), _line(line), _info(info) {}
 
+	ExceptionType getType() const { return _type; }
 	int getLine() const { return _line; }
 	const string getInfo() const { return _info; }
 };
