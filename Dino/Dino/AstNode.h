da1e32c9094902beb676223181e5bf4466de69e1
QazmoQwerty <sdonmeiri@gmail.com>
2019-08-12 01:22:43 +0300

    added some toString function in preperation for showing the ast in a .DOT file

--- Dino/Dino/AstNode.h
+++ Dino/Dino/AstNode.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <iostream>
 #include "TypeEnums.h"
 using std::string;
 using std::vector;
@@ -10,9 +11,13 @@ namespace AST
 {
 	class Node
 	{
+		unsigned int _nodeId;	// defined for purpose of the graphic view of the AST.
 	public:
+		Node(unsigned int nodeId) { _nodeId = nodeId; };
+		const unsigned int getNodeId() const { return _nodeId; };
 		virtual bool isStatement() = 0;
 		virtual bool isExpression() = 0;
+		virtual string toString() = 0;
 	};
 
 	class Statement : public Node
@@ -41,45 +46,50 @@ namespace AST
 		vector<Statement*> _statements;
 	public:
 		virtual StatementType getType() { return ST_UNKNOWN; };
+		virtual string toString() { return "<StatementBlock>"; };
 	};
 
 	class IfThenElse : public Statement
 	{
-		Expression& _condition;
-		Statement& _ifBranch;
-		Statement& _elseBranch;
+		Expression* _condition;
+		Statement* _ifBranch;
+		Statement* _elseBranch;
 
 	public:
 		virtual StatementType getType() { return ST_UNKNOWN; };
+		virtual string toString() { return "<IfThenElse>"; };
 	};
 
 	class WhileLoop : public Statement	// Add 'for' as a seperate class?
 	{
-		Expression& _condition;
-		Statement& _statement;
+		Expression* _condition;
+		Statement* _statement;
 
 	public:
 		virtual StatementType getType() { return ST_UNKNOWN; };
+		virtual string toString() { return "<While>"; };
 	};
 
 	class VariableDeclaration : public Statement
 	{
-		Identificator _id;	// Temporary
+		Identificator _varId;	// Temporary
 		VariableType _type;
 		vector<VariableModifier> _modifiers; // public, static, reactive, etc.
 
 	public:
 		virtual StatementType getType() { return ST_UNKNOWN; };
+		virtual string toString() { return "<VariableDeclaration>"; };
 	};
 
 	class Assignment : public Statement
 	{
 		AssignmentOperator _operator;
 		Identificator _left;	// Temporary
-		Expression& _right;
+		Expression* _right;
 
 	public:
 		virtual StatementType getType() { return ST_UNKNOWN; };
+		virtual string toString() { return "<Assignment>"; };
 	};
 
 	/********************** Expressions **********************/
@@ -87,40 +97,43 @@ namespace AST
 	class BinaryOperation : public Expression
 	{
 		BinaryOperator _operator;
-		Expression& _left;
-		Expression& _right;
+		Expression* _left;
+		Expression* _right;
 
 	public:
 		virtual ExpressionType getType() { return ET_UNKNOWN; };
+		virtual string toString() { return string() + "<BinaryOperator>\\n" + "TODO"; };
 	};
 
 	class UnaryOperation : public Expression
 	{
 		UnaryOperator _operator;
-		Expression& _left;
-		Expression& _right;
+		Expression* _left;
+		Expression* _right;
 
 	public:
-		virtual ExpressionType getType() { return ET_UNKNOWN; };
+		virtual string toString() { return string() + "<UnaryOperator>\\n" + "TODO"; };
 	};
 
 	class FunctionCall : public Expression
 	{
-		Identificator _id;	// Temporary
+		Identificator _functionId;	// Temporary
 		vector<Expression*> _parameters;
 
 	public:
 		virtual ExpressionType getType() { return ET_UNKNOWN; };
+		virtual string toString() { return string() + "<FunctionCall>\\n" + "TODO"; };
 	};
 
 	class ConditionalExpression : public Expression
 	{
-		Expression& _condition;
-		Expression& _ifBranch;
-		Expression& _elseBranch;
+		Expression* _condition;
+		Expression* _ifBranch;
+		Expression* _elseBranch;
 
 	public:
 		virtual ExpressionType getType() { return ET_UNKNOWN; };
+		virtual string toString() { return string() + "<ConditionaExpression>"; };
 	};
 
 	typedef struct Identificator	// Temporary
