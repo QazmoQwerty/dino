db135c87885594af6d44f41a8564e8aac477edcb
QazmoQwerty <sdonmeiri@gmail.com>
2019-08-11 19:07:05 +0300

    implemented basic preparser function (split lines)

--- Dino/Dino/Preparser.cpp
+++ Dino/Dino/Preparser.cpp
@@ -1,7 +1,30 @@
 
 #include "Preparser.h"
 
-vector<vector<Token*>>& Preparser::Preparse(vector<Token*> tokens)
+vector<vector<Token*>*>& Preparser::Preparse(vector<Token*> tokens)
 {
-	// TODO: insert return statement here
+	auto preparsedTokens = new vector<vector<Token*>*>();
+	auto currentLine = new vector<Token*>();
+
+	for (unsigned int i = 0; i < tokens.size(); i++)
+	{
+		if (tokens[i]->_type == TT_MULTI_LINE_COMMENT || tokens[i]->_type == TT_WHITESPACE)
+		{
+			delete tokens[i];
+			continue;
+		}
+		if (tokens[i]->_type == TT_SINGLE_LINE_COMMENT || tokens[i]->_type == TT_LINE_BREAK || tokens[i]->_type == TT_NEWLINE)	// TODO - make it so you can have multiple-line sections of code.
+		{
+			delete tokens[i];
+			if (currentLine->size() > 0)
+			{
+				preparsedTokens->push_back(currentLine);
+				currentLine = new vector<Token*>();
+			}
+			continue;
+		}
+		currentLine->push_back(tokens[i]);
+	}
+
+	return *preparsedTokens;
 }
--- Dino/Dino/Preparser.h
+++ Dino/Dino/Preparser.h
@@ -15,12 +15,15 @@ using std::unordered_map;
 /*
 	The Preparser gets a vector of tokens, and cleans it up to make the Parser's job easier.
 	The final result is a two dimensional vector of tokens, in which each individual vector represents a line of code.
+	Some examles of things the Preparser does:
+		1) Discards any whitespace/comment tokens.
+		2) Deals with special operators which the Lexer cannot recognise (such as word operators - if, while, else).
 */
 
 class Preparser
 {
 public:
-	static vector<vector<Token*>>& Preparse(vector<Token*> tokens);
+	static vector<vector<Token*>*>& Preparse(vector<Token*> tokens);
 private:
 
 };
