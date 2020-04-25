7ca6c6111da56c2839fba5c2c57c973d6b076777
qazmoqwerty <sdonmeiri@gmail.com>
2020-04-26 00:13:20 +0300

    fixed what I broke last commit, everything should work fine now

---
+++ src/CodeGenerator/CGGlobals.cpp
@@ -0,0 +1,80 @@
+/*
+    Definitions for all the globals declared in "CodeGenContext.h"
+*/
+#include "CodeGenerator.h"
+
+namespace CodeGenerator 
+{
+    /* Maps vtable function index information to interface's member's name. */
+    std::unordered_map<DST::InterfaceDeclaration*, std::unordered_map<unicode_string, InterfaceFuncInfo, UnicodeHasherFunction>> _interfaceVtableFuncInfo;
+
+    /* Maps DST::TypeDeclarations to useful codeGen info */
+    std::unordered_map<DST::TypeDeclaration*, TypeDefinition*> _types;
+
+    /* Maps a namespace's id to all values, types, and namespaces contained in it */
+    std::unordered_map<unicode_string, NamespaceMembers*, UnicodeHasherFunction> _namespaces;
+
+    /* Are we compiling a library or a regular file? */
+    bool _isLib;
+
+    /* Needed for any codeGen */
+    llvm::LLVMContext _context;
+    
+    /* The module we're generating IR to */
+    std::unique_ptr<llvm::Module> _module(new llvm::Module("test", _context));
+
+    /* Makes IR generation much simpler */
+    llvm::IRBuilder<> _builder(_context);
+
+    /* Used to find allocation sizes of types */
+    llvm::DataLayout *_dataLayout(new llvm::DataLayout(_module.get()));
+
+    /* All the values existing in the local scope */
+    std::unordered_map<std::string, Value*> _namedValues;
+
+    /* Since multi-return functions are not supported in LLVM we have to return them by reference (through arguments) */
+    vector<Value*> _funcReturns;
+
+    /*
+        %.interface_type = type { i8*, %.vtable_type* }
+        First member is a ptr to the object, second is a ptr to the object's vtable.
+    */
+    llvm::StructType *_interfaceType;
+
+    /*
+        %.vtable_type = type { i32, %.interface_vtable* }
+        Fist member is the type's unique ID.
+        Second member is an array containing a vtable corresponding to each interface the type implements.
+        Each interface's vtable contains that interface's ID and all the function the type implements for that interface.
+    */
+    llvm::StructType *_objVtableType;
+
+    /*
+        %.interface_vtable = type { i32, i8** }
+        First value is the interface's unique ID, second is an array of functions.
+    */
+    llvm::StructType *_interfaceVtableType;
+
+    /* Maps all types to their vtables */
+    unordered_map<llvm::Type*, llvm::Value*> _vtables;
+
+    /* The global variable in we store a pointer to an exception we are throwing */
+    llvm::GlobalVariable *_globCaughtErr;
+
+    llvm::BasicBlock *_currCatchBlock = NULL;
+
+    /* The block we should jump to should we hit a 'break' statement */
+    llvm::BasicBlock *_currBreakJmp = NULL;
+
+    /* The block we should jump to should we hit a 'continue' statement */
+    llvm::BasicBlock *_currContinueJmp = NULL;
+
+    /* A struct type representing a string, defined in Bootstrap.dino */
+    llvm::StructType *_stringTy = NULL;
+
+    /* A struct type representing a Null Pointer Error */
+    llvm::StructType *_nullPtrErrorTy = NULL;
+
+    /* The namespaces we are currently in */
+    std::vector<NamespaceMembers*> _currentNamespace;
+}
--- src/CodeGenerator/CodeGenContext.h
+++ src/CodeGenerator/CodeGenContext.h
@@ -21,7 +21,7 @@ namespace CodeGenerator
     } InterfaceFuncInfo;
 
     /* Maps vtable function index information to interface's member's name. */
-    static std::unordered_map<DST::InterfaceDeclaration*, std::unordered_map<unicode_string, InterfaceFuncInfo, UnicodeHasherFunction>> _interfaceVtableFuncInfo;
+    extern std::unordered_map<DST::InterfaceDeclaration*, std::unordered_map<unicode_string, InterfaceFuncInfo, UnicodeHasherFunction>> _interfaceVtableFuncInfo;
     
     typedef struct TypeDefinition {
         llvm::StructType *structType;
@@ -32,7 +32,7 @@ namespace CodeGenerator
     } TypeDefinition;
 
     /* Maps DST::TypeDeclarations to useful codeGen info */
-    static std::unordered_map<DST::TypeDeclaration*, TypeDefinition*> _types;
+    extern std::unordered_map<DST::TypeDeclaration*, TypeDefinition*> _types;
 
     typedef struct NamespaceMembers {
         std::unordered_map<unicode_string, llvm::Value*, UnicodeHasherFunction> values;
@@ -42,34 +42,34 @@ namespace CodeGenerator
     } NamespaceMembers;
 
     /* Maps a namespace's id to all values, types, and namespaces contained in it */
-    static std::unordered_map<unicode_string, NamespaceMembers*, UnicodeHasherFunction> _namespaces;
+    extern std::unordered_map<unicode_string, NamespaceMembers*, UnicodeHasherFunction> _namespaces;
 
     /* Are we compiling a library or a regular file? */
-    static bool _isLib;
+    extern bool _isLib;
 
     /* Needed for any codeGen */
-    static llvm::LLVMContext _context;
+    extern llvm::LLVMContext _context;
     
     /* The module we're generating IR to */
-    static std::unique_ptr<llvm::Module> _module(new llvm::Module("test", _context));
+    extern std::unique_ptr<llvm::Module> _module;
 
     /* Makes IR generation much simpler */
-    static llvm::IRBuilder<> _builder(_context);
+    extern llvm::IRBuilder<> _builder;
 
     /* Used to find allocation sizes of types */
-    static llvm::DataLayout *_dataLayout(new llvm::DataLayout(_module.get()));
+    extern llvm::DataLayout *_dataLayout;
 
     /* All the values existing in the local scope */
-    static std::unordered_map<std::string, Value*> _namedValues;
+    extern std::unordered_map<std::string, Value*> _namedValues;
 
     /* Since multi-return functions are not supported in LLVM we have to return them by reference (through arguments) */
-    static vector<Value*> _funcReturns;
+    extern vector<Value*> _funcReturns;
 
     /*
         %.interface_type = type { i8*, %.vtable_type* }
         First member is a ptr to the object, second is a ptr to the object's vtable.
     */
-    static llvm::StructType *_interfaceType;
+    extern llvm::StructType *_interfaceType;
 
     /*
         %.vtable_type = type { i32, %.interface_vtable* }
@@ -77,34 +77,34 @@ namespace CodeGenerator
         Second member is an array containing a vtable corresponding to each interface the type implements.
         Each interface's vtable contains that interface's ID and all the function the type implements for that interface.
     */
-    static llvm::StructType *_objVtableType;
+    extern llvm::StructType *_objVtableType;
 
     /*
         %.interface_vtable = type { i32, i8** }
         First value is the interface's unique ID, second is an array of functions.
     */
-    static llvm::StructType *_interfaceVtableType;
+    extern llvm::StructType *_interfaceVtableType;
 
     /* Maps all types to their vtables */
-    static unordered_map<llvm::Type*, llvm::Value*> _vtables;
+    extern unordered_map<llvm::Type*, llvm::Value*> _vtables;
 
     /* The global variable in we store a pointer to an exception we are throwing */
-    static llvm::GlobalVariable *_globCaughtErr;
+    extern llvm::GlobalVariable *_globCaughtErr;
 
-    static llvm::BasicBlock *_currCatchBlock = NULL;
+    extern llvm::BasicBlock *_currCatchBlock;
 
     /* The block we should jump to should we hit a 'break' statement */
-    static llvm::BasicBlock *_currBreakJmp = NULL;
+    extern llvm::BasicBlock *_currBreakJmp;
 
     /* The block we should jump to should we hit a 'continue' statement */
-    static llvm::BasicBlock *_currContinueJmp = NULL;
+    extern llvm::BasicBlock *_currContinueJmp;
 
     /* A struct type representing a string, defined in Bootstrap.dino */
-    static llvm::StructType *_stringTy = NULL;
+    extern llvm::StructType *_stringTy;
 
     /* A struct type representing a Null Pointer Error */
-    static llvm::StructType *_nullPtrErrorTy = NULL;
+    extern llvm::StructType *_nullPtrErrorTy;
 
     /* The namespaces we are currently in */
-    static std::vector<NamespaceMembers*> _currentNamespace;
+    extern std::vector<NamespaceMembers*> _currentNamespace;
 }
